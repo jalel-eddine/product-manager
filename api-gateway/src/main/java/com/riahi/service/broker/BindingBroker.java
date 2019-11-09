b47861037e8fffb33550f4b53f112e92832c81b1
jalel-eddine <45307944+jalel-eddine@users.noreply.github.com>
2019-11-09 21:49:18 +0100

    Delete BindingBroker.java

--- api-gateway/src/main/java/com/riahi/service/broker/BindingBroker.java
+++
@@ -1,9 +0,0 @@
-/*package com.riahi.service.broker;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(ApiStreamTopicConfig.class)
-public class BindingBroker {
-
-}
-*/
